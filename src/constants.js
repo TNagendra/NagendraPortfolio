6513af7e1fced5af349b8cec6e5248eb4995d2c5
TNagendra <nagendratangeti4846@gmail.com>
2023-06-16 14:48:27 +0530

    portifolio

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 import { BiBriefcaseAlt, BiHomeAlt } from "react-icons/bi";
 import { FiUser, FiMail } from "react-icons/fi";
-import nxtwatch from "./assests/nxtwatch.png";
+import nxtWatch from "./assests/nxtWatch.png";
 import movieBooking from "./assests/movie-booking-app.png";
+import foodMunch from "./assests/foodMunch.png";
+import jobbyApp from "./assests/jobbyApp.png";
 
 export const navItems = [
   {
@@ -28,10 +30,10 @@ export const projects = [
     name: "Nxt Watch",
     subtitle: "Youtube Clone | React Application",
     description: `Watch Trending Videos | Save videos to watch later | Find videos in topics and categories | Give the video a thumbs up or a thumbs down depending on whether you like it or not`,
-    url: "https://extraordinary-manatee-3ba453.netlify.app/",
+    url: "https://648be61e1ebecc2b22dd5bb6--benevolent-gumption-624532.netlify.app/login",
     pic: (
       <img
-        src={nxtwatch}
+        src={nxtWatch}
         alt="NxtWatch-Project"
         className="col-12 col-md-9 col-lg-12"
       />
@@ -48,6 +50,34 @@ export const projects = [
       <img
         src={movieBooking}
         alt="Movie-Tickets-Booking-App"
+        className="col-12 col-md-9 col-lg-12"
+      />
+    ),
+  },
+  {
+    id: 3,
+    name: "Jobby App",
+    subtitle: "Job Demo | React Application",
+    description: `Jobby helps to search the relevant jobs and you can filter the salary range | Used React.js for frontend, Node.js and Express.js for handling REST API calls.`,
+    url: "https://648bf81b7897082fe1307740--wonderful-swan-290419.netlify.app/",
+    pic: (
+      <img
+        src={jobbyApp}
+        alt="Jobby-App"
+        className="col-12 col-md-9 col-lg-12"
+      />
+    ),
+  },
+  {
+    id: 4,
+    name: "Food Munch Demo",
+    subtitle: "Food App Demo",
+    description: `Developed a responsive website for Food Store where users can see list of food items & information.`,
+    url: "https://yashfoodsite.ccbp.tech/",
+    pic: (
+      <img
+        src={foodMunch}
+        alt="Food-Munch"
         className="col-12 col-md-9 col-lg-12"
       />
     ),
